9edc6f8614f16d0dc7c24ca7d07d69c84bcada23
Zanab Shah <zanab@protonmail.com>
2024-08-25 20:31:04 -0700

    added required to comment placeholder

--- src/components/AddComment/AddComment.jsx
+++ src/components/AddComment/AddComment.jsx
@@ -9,7 +9,7 @@ export const AddComment = ({userImageSrc}) => {
             <div className='add-comment__conversation'>
                 <h3>JOIN THE CONVERSATION</h3>
                 <div className='add-comment__field'>
-                    <textarea placeholder='Add a new comment' className='add-comment__input'></textarea>
+                    <textarea required placeholder='Add a new comment' className='add-comment__input'></textarea>
                     <Button prefixSrc={'/src/assets/images/icons/add_comment.svg'}>COMMENT</Button>
                 </div>
             </div>
